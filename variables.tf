58e796c072bf4a51c6a4d0c0ea81513fc324b935
Johnny <joaogbdesouza@gmail.com>
2025-06-24 22:08:02 -0300

    adicionando nós no cluster

--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "prefix" {}
 variable "cluster_name" {
   default = "terraform-basic-cluster"
 }
+
+variable "desired_size" {
+  default = 2
+}
+
+variable "max_size" {
+  default = 3
+}
+
+variable "min_size" {
+  default = 1
+}
