306eabf9107131796cd670788dd607b35a87f357
Johnny <joaogbdesouza@gmail.com>
2025-06-26 21:05:18 -0300

    passando a vpc prum módulo

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,10 +8,10 @@ resource "aws_vpc" "vpc" {
 data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "subnets" {
-  count = 2
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.0.${count.index}.0/24"
+  count                   = 2
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.${count.index}.0/24"
   map_public_ip_on_launch = true
   tags = {
     Name = "${var.prefix}-subnet-${count.index}"
@@ -37,10 +37,10 @@ resource "aws_route_table" "route_table" {
 }
 
 resource "aws_route_table_association" "route_table_association" {
-  count = 2
-  subnet_id = aws_subnet.subnets.*.id[count.index]
+  count          = 2
+  subnet_id      = aws_subnet.subnets.*.id[count.index]
   route_table_id = aws_route_table.route_table.id
-  
+
 }
 
 # resource "aws_subnet" "terraform_subnet_2" {
